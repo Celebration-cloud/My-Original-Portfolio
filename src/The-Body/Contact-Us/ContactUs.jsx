1d0dd83098c087f644202797eedee3c0b9860a10
Celebration-cloud <ojinguluc@gmail.com>
2023-10-22 18:37:27 +0100

    my profile

---
+++ src/The-Body/Contact-Us/ContactUs.jsx
@@ -0,0 +1,19 @@
+import GetInTouch from "./GetInTouch";
+import Support from "./Support";
+import './ContactUs.css'
+function ContactUs() {
+  return (
+    <div>
+      <h4 id="scrollspyHeading4">CONTACT US</h4>
+      <br />
+      <div className="conUs container">
+        <div className="row">
+          <Support/>
+          <GetInTouch/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs
---
+++ src/The-Body/TheBody.jsx
@@ -0,0 +1,30 @@
+import AboutUs from "./About-Us/AboutUs";
+import ContactUs from "./Contact-Us/ContactUs";
+import OurSkill from "./Our-Skills/OurSkill";
+import Remark from "./Remark/Remark";
+import OurTeam from "./The-Team/OurTeam";
+
+function TheBody() {
+  return (
+    <div>
+      <div
+        data-bs-spy="scroll"
+        data-bs-target="#navbar-example2"
+        data-bs-offset="0"
+        className="scrollspy-example"
+        tabIndex="0"
+      >
+        <AboutUs/>
+        <OurTeam/>
+        <OurSkill/>
+        <Remark/>
+        <OurServices />
+        <h4 id="scrollspyHeading3">Portfolio</h4>
+        <p>...</p>
+        <ContactUs/>
+      </div>
+    </div>
+  );
+}
+
+export default TheBody
